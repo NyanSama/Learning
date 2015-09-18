e1acd9f0a64306c345ae04fa8dabcf93180d3e4c
NyanSama <NyanSama@163.com>
2015-09-18 16:03:37 +0800

    for while practise1

---
+++ Python/7.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+L = ['Bart', 'Lisa', 'Adam']
+
+print("Using for:")
+for name in L:
+    print(name)
+print("Using while:")
+i = 0
+while i < len(L):
+    print(L[i])
+    i += 1
